16eca515f3b821f4fa67e2e4439b34b1c70f3065
Thanh Hoang <thanhhoang@Mac.local>
2024-02-06 23:35:42 +0700

    test

--- src/config.js
+++ src/config.js
@@ -1,21 +1,21 @@
-export const NetworkId = 'testnet';
+export const NetworkId = "testnet";
 
 export const HelloNearContract = {
-  mainnet: 'hello.near-examples.near',
-  testnet: 'hello.near-examples.testnet',
-}
-
+  mainnet: "hello.near-examples.near",
+  testnet: "hello.near-examples.testnet",
+};
+// n
 export const ComponentMap = {
   mainnet: {
-    socialDB: 'social.near',
-    Lido: 'zavodil.near/widget/Lido',
-    HelloNear: 'gagdiez.near/widget/HelloNear',
-    LoveNear: 'gagdiez.near/widget/LoveNear',
+    socialDB: "social.near",
+    Lido: "zavodil.near/widget/Lido",
+    HelloNear: "gagdiez.near/widget/HelloNear",
+    LoveNear: "gagdiez.near/widget/LoveNear",
   },
   testnet: {
-    socialDB: 'v1.social08.testnet',
-    Lido: 'influencer.testnet/widget/Lido',
-    HelloNear: 'influencer.testnet/widget/HelloNear',
-    LoveNear: 'influencer.testnet/widget/LoveNear',
-  }
-} 
+    socialDB: "v1.social08.testnet",
+    Lido: "influencer.testnet/widget/Lido",
+    HelloNear: "influencer.testnet/widget/HelloNear",
+    LoveNear: "influencer.testnet/widget/LoveNear",
+  },
+};
